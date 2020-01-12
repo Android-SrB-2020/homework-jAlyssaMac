8fba7ae1c9562c2df0abd1b17566e383c1d7b1e8
owlgirl07 <jessica.alyssa.07@live.ca>
2020-01-12 11:01:13 -0400

    Finished 1.3

--- 1 First Apps/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ 1 First Apps/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
@@ -22,31 +29,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-//        Toast.makeText(this, "button clicked",
-//        Toast.LENGTH_SHORT).show()
-        val randomInt = Random().nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
 
+    private fun getRandomDiceImage() : Int {
+
+        return when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
     }
 
     private fun countUp(){
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        // If text is the default "Hello World!" set that text to 1.
-        if (resultText.text == "Hello World") {
-            resultText.text = "1"
-        } else {
-            // Otherwise, increment the number up to 6.
-            // The text value in resultText.text is an instance of the CharSequence class;
-            // it needs to be converted to a String object before it can be converted to an int.
-            var resultInt = resultText.text.toString().toInt()
-
-            if (resultInt < 6) {
-                resultInt++
-                resultText.text = resultInt.toString()
-            }
-        }
+//        val resultText: TextView = findViewById(R.id.result_text)
+//
+//        // If text is the default "Hello World!" set that text to 1.
+//        if (resultText.text == "Hello World") {
+//            resultText.text = "1"
+//        } else {
+//            // Otherwise, increment the number up to 6.
+//            // The text value in resultText.text is an instance of the CharSequence class;
+//            // it needs to be converted to a String object before it can be converted to an int.
+//            var resultInt = resultText.text.toString().toInt()
+//
+//            if (resultInt < 6) {
+//                resultInt++
+//                resultText.text = resultInt.toString()
+//            }
+//        }
 
 
     }
