588c7b9e735011d455e61c3f97974d39ec8db8bd
owlgirl07 <jessica.alyssa.07@live.ca>
2020-01-27 10:07:49 -0400

    2020-01-27

--- 1 First Apps/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ 1 First Apps/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -9,33 +10,37 @@ import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
+val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage : ImageView
     lateinit var diceImage2 : ImageView
+    lateinit var diceImage3 : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d(TAG, "In onCreate")
+
         diceImage = findViewById(R.id.dice_image)
         diceImage2 = findViewById(R.id.dice_image2)
+        diceImage3 = findViewById(R.id.dice_image3)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-
-        val CountUpButton: Button = findViewById(R.id.countup_button)
-        CountUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
         diceImage.setImageResource(getRandomDiceImage())
         diceImage2.setImageResource(getRandomDiceImage())
+        diceImage3.setImageResource(getRandomDiceImage())
     }
 
     private fun getRandomDiceImage() : Int {
 
-        return when (Random().nextInt(6) + 1) {
+        return when ((1..6).random()){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -43,28 +48,21 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-    }
 
-    private fun countUp(){
-//        val resultText: TextView = findViewById(R.id.result_text)
-//
-//        // If text is the default "Hello World!" set that text to 1.
-//        if (resultText.text == "Hello World") {
-//            resultText.text = "1"
-//        } else {
-//            // Otherwise, increment the number up to 6.
-//            // The text value in resultText.text is an instance of the CharSequence class;
-//            // it needs to be converted to a String object before it can be converted to an int.
-//            var resultInt = resultText.text.toString().toInt()
-//
-//            if (resultInt < 6) {
-//                resultInt++
-//                resultText.text = resultInt.toString()
-//            }
+//        return when (Random().nextInt(6) + 1) {
+//            1 -> R.drawable.dice_1
+//            2 -> R.drawable.dice_2
+//            3 -> R.drawable.dice_3
+//            4 -> R.drawable.dice_4
+//            5 -> R.drawable.dice_5
+//            else -> R.drawable.dice_6
 //        }
-
-
     }
+
+
+
+
+
 
 
 }
